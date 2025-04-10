49945d5503b77188f93493af4a6d84efeae799a3
ame133 <ame1117c@gmail.com>
2025-04-10 14:26:58 -0600

    style

--- DINAMOFRONT/dinamo/src/pages/HomePage.jsx
+++ DINAMOFRONT/dinamo/src/pages/HomePage.jsx
@@ -1,13 +1,20 @@
-import './HomePage.css'; // Asegúrate de importar el archivo CSS
+import './HomePage.css';
 import logo from '../assets/gif.gif';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/login'); // Redirige a la ruta /login
+  };
+
   return (
     <div className="home-container">
       <div className="content">
-      <img src={logo} alt="Logo" className="logo-img" />
+        <img src={logo} alt="Logo" className="logo-img" />
         <h1>DINAMO</h1>
-        
+        <button onClick={handleClick}>Iniciar</button>
       </div>
     </div>
   );
